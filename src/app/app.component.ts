648ecc3eef42b1291503d3f861f64eb8a73cb0ae
LesPo <139312751+LeonardoPove@users.noreply.github.com>
2024-08-05 21:53:11 -0500

    se creo el portafolio v2 para empezarlo mobile y asi seria mas facil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BodyComponent } from './componets/body/body.component';
+import { PerfilComponent } from './componets/perfil/perfil.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    BodyComponent,
+    PerfilComponent,
+    HeaderComponent,
+    SidenavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'sobremi', loadComponent: () => import('./componets/sobremi/sobremi.component').then(m => m.SobremiComponent) },
+    { path: '', redirectTo: '/about', pathMatch: 'full' },
+    { path: '**', redirectTo: '/about' }
+];  
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { } 
